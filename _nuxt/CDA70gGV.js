d7cdf32aeddec5391495482234b1f646ab324c27
gabe-crafting <gabriel.projects90@proton.me>
2025-07-17 18:58:01 +0000

    Deploying to gh-pages from @ gabe-crafting/portfolio-cv@32f7ad692bfb2ad6f5cd188dfd129fbbca4525c3 🚀

--- _nuxt/CDA70gGV.js
+++
@@ -1,1 +0,0 @@
-import{y as r,O as c,c as l,h as p,C as u,n as d,aN as s}from"./DPFjAART.js";const i={base:""},y={__name:"ProseTbody",props:{class:{type:null,required:!1}},setup(e){const a=e,o=r(),t=c(()=>s({extend:s(i),...o.uiPro?.prose?.tbody||{}}));return(n,m)=>(p(),l("tbody",{class:d(t.value({class:a.class}))},[u(n.$slots,"default")],2))}};export{y as default};
