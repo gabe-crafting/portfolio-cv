b1abfe90b4e6df256b63862a407faed67c95e892
gabe-crafting <gabriel.projects90@proton.me>
2025-07-17 18:05:00 +0000

    Deploying to gh-pages from @ gabe-crafting/portfolio-cv@524aed3011b2efef1afaa25ea73ddf6961883103 🚀

---
+++ _nuxt/DRpq9Oda.js
@@ -0,0 +1,1 @@
+import{r as t}from"./CrZ4pUTn.js";import e from"./CSEn0VJO.js";import{y as n,B as s,h as a,w as i,E as p}from"./B1K6FPZA.js";import"./iik6CYzq.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
