439dd03aa61baee44ffabdfaf06e8d17dec8a611
gabe-crafting <gabriel.projects90@proton.me>
2025-12-04 17:12:31 +0000

    Deploying to gh-pages from @ gabe-crafting/portfolio-cv@1ac77023fd0ac4963d38d2b75bd881ea8b9f50f3 🚀

---
+++ _nuxt/B_oFJnJW.js
@@ -0,0 +1,1 @@
+import{r as a}from"./Cxt2CXBZ.js";import t from"./DFpRzWvK.js";import{y as e,B as n,h as s,w as c,E as i}from"./DCb9Yyuc.js";const l={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(t,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
